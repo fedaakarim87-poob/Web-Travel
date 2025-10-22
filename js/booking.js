004f448e457150ebba93da7fa03e5d732d309401
ghaida hajar <ghaidahajar5@gmail.com>
2025-10-22 21:07:51 +0300

    Update booking.js

--- js/booking.js
+++ js/booking.js
@@ -1,1 +1,46 @@
+//   الصفحه الرئيسيه
+const modal = document.getElementById('bookingModal');
+const destinationName = document.getElementById('destinationName');
+const bookingForm = modal.querySelector('form');
 
+
+let confirmMessage = document.getElementById("confirmBookingMessage");
+if (!confirmMessage) {
+    confirmMessage = document.createElement("p");
+    confirmMessage.id = "confirmBookingMessage";
+    confirmMessage.style.marginTop = "10px";
+    confirmMessage.style.fontWeight = "bold";
+    modal.querySelector(".modal-content").appendChild(confirmMessage);
+}
+function openBookingModal(destination) {
+    destinationName.textContent = destination;
+    modal.style.display = 'block';
+    confirmMessage.textContent = "";
+    bookingForm.reset();
+}
+function closeBookingModal() {
+    modal.style.display = 'none';
+    confirmMessage.textContent = "";
+    bookingForm.reset();
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        confirmMessage.textContent = "تم إرسال طلبك ";
+        confirmMessage.style.color = "blue";
+        setTimeout(() => {
+            confirmMessage.textContent = "";
+        }, 3000);
+        bookingForm.reset();
+    }
+}
+bookingForm.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+
+    confirmMessage.textContent = "تم تأكيد الطلب بنجاح ";
+    confirmMessage.style.color = "blue";
+    setTimeout(() => {
+        bookingForm.reset();
+        confirmMessage.textContent = "";
+    }, 3000);
+}); 
